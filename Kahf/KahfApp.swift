5e5b312e85a141297b765a0d57b18e36337a7d1d
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-10-09 18:48:16 +0600

    Initial Commit

---
+++ Kahf/KahfApp.swift
@@ -0,0 +1,17 @@
+//
+//  KahfApp.swift
+//  Kahf
+//
+//  Created by Mohammed Rokon Uddin on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct KahfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
